b09bc3173eeea6b6345a837a6454e2f388c2a610
王旭 <wangxu@jybdshop.cn>
2018-04-25 14:36:18 +0800

    测试

---
+++ app/src/main/java/test/com/testforkpro/Test.java
@@ -0,0 +1,14 @@
+package test.com.testforkpro;
+
+/**
+ * @Description:
+ * @Author:王旭
+ * @CreatTime:2018/4/25 0025
+ * @LastModify(最终修改人):王旭
+ * @LastModifyTime(最终修改时间):2018/4/25 0025
+ * @LastChekedBy: 王旭
+ * @needingAttention(注意事项):
+ */
+
+public class Test {
+}
